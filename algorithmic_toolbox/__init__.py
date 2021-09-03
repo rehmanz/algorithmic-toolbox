26e3741945e26e241f8b4d7fb450ba57c6ec6f22
rehmanz <rehmanz@yahoo.com>
2021-09-03 11:08:58 -0700

    Code cleanup.

--- algorithmic_toolbox/__init__.py
+++ algorithmic_toolbox/__init__.py
@@ -3,8 +3,6 @@ __version__ = "0.1.0"
 """Return max pairwise product
 
 """
-
-
 def max_pairwise_product(numbers):
     sorted_numbers = sorted(map(int, numbers.split()))
     n = len(sorted_numbers)
