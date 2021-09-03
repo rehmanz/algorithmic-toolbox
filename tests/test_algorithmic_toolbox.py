05a54e52758303cc8cd46b18d4da9da2e72d7225
rehmanz <rehmanz@yahoo.com>
2021-09-03 09:01:44 -0700

    Code cleanup.

--- tests/test_algorithmic_toolbox.py
+++ tests/test_algorithmic_toolbox.py
@@ -1,7 +1,9 @@
 from algorithmic_toolbox import __version__, max_pairwise_product
 
+
 def test_version():
     assert __version__ == "0.1.0"
+
 
 def test_max_pairwise_product():
     assert max_pairwise_product("4 7 1 9") == 63
