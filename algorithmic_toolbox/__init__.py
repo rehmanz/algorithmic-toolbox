54870754d0eef28a6871bc5982d4e8a613655863
rehmanz <rehmanz@yahoo.com>
2021-09-05 09:02:44 -0700

    Adding pylint.

--- algorithmic_toolbox/__init__.py
+++ algorithmic_toolbox/__init__.py
@@ -1,11 +1,15 @@
-__version__ = "0.1.0"
-
-"""Return max pairwise product
-
+"""Algorithmic toolbox module contains simple algorithms.
 """
+
+__version__ = "0.1.0"
 
 
 def max_pairwise_product(numbers):
+    """Return max pairwise product.
+
+    Keyword arguments:
+    numbers -- string containing numbers
+    """
     sorted_numbers = sorted(map(int, numbers.split()))
-    n = len(sorted_numbers)
-    return sorted_numbers[n - 1] * sorted_numbers[n - 2]
+    num_len = len(sorted_numbers)
+    return sorted_numbers[num_len - 1] * sorted_numbers[num_len - 2]
